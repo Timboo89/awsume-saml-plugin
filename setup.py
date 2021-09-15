d6fe8e3ef0c6b7310aea223560feb01495800454
Timo Schmidt <t.schmidt@reply.de>
2021-09-15 12:33:14 +0200

    adding wheel and dependencies

--- setup.py
+++ setup.py
@@ -1,6 +1,9 @@
-from setuptools import setup
+import setuptools
 
-setup(
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
     name='awsume-saml-plugin',
     version='0.1.0',
     entry_points={
@@ -9,6 +12,16 @@ setup(
         ]
     },
     author='Timo Schmidt',
-    author_email='bsc.timo.schmitd@gmail.com',
+    author_email='bsc.timo.schmidt@gmail.com',
     py_modules=['saml'],
+    description='Plugin for awsume assuming AWS roles with SAML federation.',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Timboo89/awsume-saml-plugin",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache License 2.0",
+        "Operating System :: OS Independent",
+    ],
 )
